6b2d9f5eccd4b334fa51091ed011b53f7335ddec
Aidan Lewis <aidan@axiomalaska.com>
2023-05-25 15:36:28 -0800

    fix helper script, split erddap deployment into its own file

--- deploy/gcp/prod/copy_datasets.sh
+++ deploy/gcp/prod/copy_datasets.sh
@@ -12,3 +12,28 @@ for f in "$(pwd)"/dev/pvc/data/erddap/datasets.d/*; do
     echo "Running kubectl cp $f $pod:/datasets.d/$filename"
     kubectl cp $f $pod:/datasets.d/$filename
 done
+
+
+kubectl exec -it $pod -- bash -c "
+
+    cd /erddapData
+
+    mkdir -p storage/iot-data-landing/MockCo
+
+    cd storage/iot-data-landing/MockCo
+
+    dirs=("Sensor-1" "Sensor-1_QC" "Sensor-2" "Sensor-2_QC")
+
+    for dir in \${dirs[@]}; do
+        mkdir \$dir
+    done
+
+    exit
+"
+
+dirs=("Sensor-1" "Sensor-1_QC" "Sensor-2" "Sensor-2_QC")
+
+for f in ${dirs[@]}; do
+    echo "Running kubectl cp \"$(pwd)\"/dev/pvc/data/erddap/bootstraps/$f.jsonl $pod:/erddapData/storage/iot-data-landing/MockCo/$f/bootstrap.jsonl"
+    kubectl cp "$(pwd)"/dev/pvc/data/erddap/bootstraps/$f.jsonl $pod:/erddapData/storage/iot-data-landing/MockCo/$f/bootstrap.jsonl
+done
